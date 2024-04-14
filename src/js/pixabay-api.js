61f885a53e209f78adf541b7227a3e2de4daa071
Elen Gaslin <lenokverba@gmail.com>
2024-04-14 09:27:58 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 export function fetchSearchData(inputValue) {
   return fetch(
-    `https://pixabay.com/api?key=43344666-8172f6ac72992b92fe58b4040&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
+    `https://pixabay.com/api/?key=43344666-8172f6ac72992b92fe58b4040&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
